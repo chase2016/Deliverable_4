d75f0d7065d6cc3a0190337f0da7493facadab54
chase2016 <chase.raymond22@gmail.com>
2016-07-13 17:55:28 -0400

    Add files via upload

---
+++ billifytest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class billifytest {
+	
+	// Test will verify the square of the number happens
+	@Test
+	public void SquareTest() {
+		for(int i = 0; i < 100; i++) {
+			int[] arr = outputArray();
+			int[] billifyarr = billify.billify(arr);		
+			for(int i1 = 0; i1 < billifyarr.length - 1; i1++) {
+				assertEquals(billifyarr[i1], arr[i1] * arr[i1]);
+			}
+		}
+		}
+
+	
+	//test runs if the array will work
+    @Test
+    public void RunTest() throws Exception {
+    	assertArrayEquals(new int[]{1,2,3},new int[]{1,2,3});
+
+    }
+
+    
+    //Test will verify the the output will always be larger
+    @Test
+	public void LargerTest() {
+		for(int i = 0; i < 100; i++) {
+			int[] arr = outputArray();
+			int[] billifyarr = billify.billify(arr);		
+			for(int i1 = 0; i1 < arr.length; i1++) {
+				if(billifyarr[i1] > billifyarr[arr.length]) {
+					fail();
+				}
+			}
+		}
+	}
+
+	private int[] outputArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+  
+  
+}
